bd99846305196c51eb95e3db075586b49c002970
Jachin Rupe <jachin.rupe@gmail.com>
2021-06-01 17:08:06 -0500

    Adding an actual test.

--- cypress/integration/option_groups.spec.js
+++ cypress/integration/option_groups.spec.js
@@ -2,6 +2,12 @@ describe("My First Test", () => {
   it("Visits the Kitchen Sink", () => {
     cy.visit("http://localhost:1234");
 
-    cy.contains("Option Groups");
+    cy.get("#option-groups").click();
+
+    cy.get("#option-groups").shadow().contains("Yellow").click();
+
+    cy.get("#option-groups")
+      .invoke("attr", "selected-value")
+      .should("eq", "Yellow");
   });
 });
